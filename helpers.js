12dbad997881d28d72587ae79526287db272bac3
kalippanser-minnuz <94594428+kalippanser-minnuz@users.noreply.github.com>
2021-11-18 22:59:04 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *Oggy_ser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *Oggy_ser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *Oggy_ser*:  ```" + msg + "```"
 }
 
 
